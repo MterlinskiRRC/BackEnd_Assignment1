8d9630f6df77122080361eca26683bc92da11a01
“Matthew <Mterlinski@rrc.ca>
2026-01-19 12:19:26 -0600

    Made changes to the server config to get it working

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
-import app from './app';
+import app from "./app";
+import { Server } from "http";
 
-const port = process.env.PORT || 3000;
+const PORT: string | number = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const server: Server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+export { server };
